b2d89933c72abd977c01137aab5599ff3f458bf6
Nalin Dahyabhai <nalin@redhat.com>
2012-08-30 18:28:39 -0400

    give each test its own set of ports

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -15,6 +15,9 @@ test -n "$kadmind" && echo Using kadmind binary: $kadmind
 test -n "$kadmin"  && echo Using kadmin.local binary: $kadmin
 
 # Run each test with clear log files and a fresh copy of the KDC and kadmind.
+kdcport=`expr $RANDOM % 8800 + 8800`
+kadminport=`expr $kdcport + 1`
+kpasswdport=`expr $kadminport + 1`
 for test in ${@:-"$testdir"/0*} ; do
 	if ! test -s $test/run.sh ; then
 		continue
@@ -22,7 +25,16 @@ for test in ${@:-"$testdir"/0*} ; do
 	echo -n `basename "$test"` ..." "
 	test_kdcinitdb
 	test_kdcprep
+	sed -i -e s/8800/$kdcport/g config/*.conf
+	sed -i -e s/8801/$kadminport/g config/*.conf
+	sed -i -e s/8802/$kpasswdport/g config/*.conf
 	meanwhile "$run_kdc" "$run_kadmind" "$test/run.sh" > $test/stdout 2> $test/stderr
+	sed -i -e s/$kdcport/8800/g config/*.conf
+	sed -i -e s/$kadminport/8801/g config/*.conf
+	sed -i -e s/$kpasswdport/8802/g config/*.conf
+	kdcport=`expr $kdcport + 3`
+	kadminport=`expr $kdcport + 1`
+	kpasswdport=`expr $kadminport + 1`
 	if test -s $test/stdout.expected ; then
 		if ! cmp -s $test/stdout.expected $test/stdout ; then
 			echo ""
