2d1a1e885d77778597bfb1949736302120d65afc
Nalin Dahyabhai <nalin@dahyabhai.net>
2012-09-01 07:34:33 -0400

    more tests, including that we know how to get armor

---
+++ tests/012-armor/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+. $testdir/testenv.sh
+
+$kadmin -q 'cpw -pw foo '$test_principal 2> /dev/null > /dev/null
+$kadmin -q 'modprinc -pwexpire never '$test_principal 2> /dev/null > /dev/null
+
+# Anonymous PKINIT armor.
+echo ""
+test_run -auth -session -run grepenvc.sh -authtok foo $test_principal $pam_krb5 $test_flags use_first_pass preauth_options=X509_anchors=FILE:$testdir/kdc/ca.crt debug armor armor_strategy=pkinit
+
+# Use a keytab.
+echo ""
+test_run -auth -session -run grepenvc.sh -authtok foo $test_principal $pam_krb5 $test_flags use_first_pass keytab=$testdir/kdc/krb5.keytab debug armor armor_strategy=keytab
